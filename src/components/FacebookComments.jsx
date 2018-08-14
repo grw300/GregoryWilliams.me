b90ca9acde51b7723bb577d5e78d4777cb645d3c
Gregory Williams <grw300@gmail.com>
2018-08-14 18:37:21 -0500

    Turn facebook comments into const; maybe this will help with consistency

--- src/components/FacebookComments.jsx
+++ src/components/FacebookComments.jsx
@@ -3,19 +3,16 @@ import FacebookProvider, { Comments } from "react-facebook";
 import urljoin from "url-join";
 import config from "../../data/SiteConfig";
 
-class FacebookComments extends React.Component {
-    render() {
-        const { postNode } = this.props;
-        if (!config.siteFBAppID) {
-            return null;
-        }
-        const url = urljoin(config.siteUrl, postNode.fields.slug);
-        return (
-            <FacebookProvider appId={config.siteFBAppID}>
-                <Comments href={url} width="100%" />
-            </FacebookProvider>
-        );
-    }
-}
+const FacebookComments = props => {
+    const { postNode } = props;
+
+    const url = urljoin(config.siteUrl, postNode.fields.slug);
+
+    return (
+        <FacebookProvider appId={config.siteFBAppID}>
+            <Comments href={url} width="100%" />
+        </FacebookProvider>
+    );
+};
 
 export default FacebookComments;
