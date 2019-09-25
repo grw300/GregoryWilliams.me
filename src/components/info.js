d923b14c07c8fce91c297d69f35bde22bcb228f6
Gregory Williams <grw300@gmail.com>
2019-09-24 21:16:33 -0500

    Add classes, re-work copy.

--- src/components/info.js
+++ src/components/info.js
@@ -11,16 +11,14 @@ export default function info() {
   return (
     <div className="container">
       <div className="my-5"></div>
-      <div className="display-3" style={{ color: "#BF5700" }}>
+      <div className="display-3 info" style={{ color: "#BF5700" }}>
         <span style = {{color : "white"}}>Gregory Williams</span> <br />
         Software Engineer
       </div>
-      <div className="text-muted mx-5 my-4 h3 text-justify info">
-        Software Engineer and Physics enthusiast.
-        <br /> 
-        Husband and father living in Austin, TX.
+      <div className="text-muted mx-5 my-4 h3 code">
+        Husband, father, and physics enthusiast living in Austin, TX.
       </div>
-      <div className="h1 mt-5">
+      <div className="h1 mt-5 social">
         <a className="mr-5 icon" href="https://twitter.com/GregWill">
           <Twitter />
         </a>
